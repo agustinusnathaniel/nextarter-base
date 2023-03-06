11b5f48603264427becd80e00432d32cb954199a
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-03-07 00:04:52 +0700

    chore: update next-seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -3,12 +3,12 @@ const defaultSEOConfig = {
   title: "nextarter-base",
   titleTemplate: "%s | nextarter-base",
   defaultTitle: "nextarter-base",
-  description: "Next.js + chakra-ui + TypeScript template",
+  description: "Next.js + TypeScript template",
   canonical: "https://nextarter-base.sznm.dev",
   openGraph: {
     url: "https://nextarter-base.sznm.dev",
     title: "nextarter-base",
-    description: "Next.js + chakra-ui + TypeScript template",
+    description: "Next.js + TypeScript template",
     images: [
       {
         url: "https://og-image.sznm.dev/**nextarter-base**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
