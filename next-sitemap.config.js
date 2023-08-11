a6fc4df321335e8dd0245818aae2e7041996ece4
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-08-11 12:40:56 +0700

    chore: remove unused files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-  siteUrl: "https://nextarter-base.sznm.dev",
+  siteUrl: 'https://nextarter-base.sznm.dev',
   generateRobotsTxt: true,
 };
 
